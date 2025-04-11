e9f362ac327ed49a3f06090e8589d9305b63ac48
alexander <uresaleksandr770@gmail.com>
2025-04-11 17:00:14 +0300

    все исправлено (ура-ура вперед-вперед nature&science 24/7)

--- src/commands/InsertCommand.java
+++ src/commands/InsertCommand.java
@@ -25,14 +25,44 @@ public class InsertCommand implements Commands{
      */
     @Override
     public void execute(String[] args){
+        Route routeToAdd;
         if (args.length > 0) {
-            System.err.println("Ошибка: укажите правильно команду");
-            return;
+            try{
+                String[] information = args[0].split(";");
+                if (information.length < 11){
+                    System.err.println("Error: недостаточно данных для insert или Вы ввели команду неверно");
+                    return;
+                }
+                Long id = routeManager.getLastID() + 1;
+                String name = information[0];
+                int x = Integer.parseInt(information[1]);
+                float y = Float.parseFloat(information[2]);
+                double fromX = Double.parseDouble(information[3]);
+                int fromY = Integer.parseInt(information[4]);
+                String fromName = information[5];
+                Long toX = Long.parseLong(information[6]);
+                Integer toY = Integer.parseInt(information[7]);
+                double toZ = Double.parseDouble(information[8]);
+                String toName = information[9];
+                double distance = Double.parseDouble(information[10]);
+
+                routeToAdd = new Route(id, name, new Coordinates(x, y), new LocationFrom(fromX, fromY, fromName), new LocationTo(toX, toY, toZ, toName), distance);
+                routeManager.addRoute(routeToAdd);
+            } catch (Exception e){
+                System.err.println("Скрипт написан неверно");
+                return;
+            }
+        } else {
+            Long idLast = routeManager.getLastID();
+            routeToAdd = inputNewRoute(idLast);
+            if (routeToAdd == null){
+                System.err.println("Маршрут не создан");
+                return;
+            }
+            routeToAdd.setId(routeToAdd.getId() + 1);
+            routeManager.addRoute(routeToAdd);
+            System.out.println("Маршрут добавлен: " + routeToAdd + ".");
         }
-        Long idLast = routeManager.getLastID();
-        Route routeToAdd = inputNewRoute(idLast);
-        routeToAdd.setId(routeToAdd.getId() + 1);
-        routeManager.addRoute(routeToAdd);
-        System.out.println("Маршрут добавлен: " + routeToAdd + ".");
+
     }
 }
--- src/models/Route.java
+++ src/models/Route.java
@@ -85,155 +85,159 @@ public class Route implements Comparable<Route>{
      * @param idLast значение предыдущего ID
      * @return Новый маршрут с новым ID = idLast + 1
      */
-    public static Route inputNewRoute(Long idLast){
+    public static Route inputNewRoute(Long idLast) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите название маршрута: ");
-        String name = scanner.nextLine().trim();
-        while(name.isEmpty()) {
-            System.out.println(("Error: Название не может быть пустым. Введите снова: "));
-            name = scanner.nextLine();
-        }
-        int x;
-
-        System.out.print("Введите координату X (целое число > -372): ");
-        while (true) {
-            try {
-                x = scanner.nextInt();
-                if (x <= -372) {
-                    System.err.print("Число должно быть больше -372. Попробуйте снова.");
-                    continue;
-                }
-                System.out.println("Вы ввели корректное число: " + x);
-                break;
-            } catch (InputMismatchException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
-                scanner.nextLine();
-            }
-        }
-        float y ;
-        System.out.print("Введите координату Y (целое число > -527): ");
-        while (true) {
-            try {
-                y = scanner.nextFloat();
-                if (y <= -527) {
-                    System.err.println("Число должно быть больше -527. Попробуйте снова.");
-                    continue;
-                }
-                System.out.println("Вы ввели корректное число: " + y);
-                break;
-            }
-            catch (InputMismatchException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
-                scanner.nextLine();
-            }
-        }
-        Coordinates coordinates2 = new Coordinates(x, y);
-        System.out.print("Введите координату from (X): ");
-        double fromX ;
-        while (true) {
-            try {
-                fromX = scanner.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
-                scanner.nextLine();
-            }
-        }
-        System.out.print("Введите координату from (Y): ");
-        int fromY;
-        while (true) {
-            try {
-                fromY = scanner.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
-                scanner.nextLine();
-            }
-        }
-        System.out.print("Введите точку начала маршрута (до 956 символов): ");
-        scanner.nextLine();
-        String fromName = scanner.nextLine().trim();
-        while (fromName.isEmpty() || (fromName.length() > 956)) {
-            System.err.print("Ошибка! Введите корректное точку начала маршрута: ");
-            fromName = scanner.nextLine();
-        }
-        LocationFrom locationFrom = new LocationFrom(fromX, fromY, fromName);
-        long toXcoordinate;
-        System.out.print("Введите координату to (X): ");
-        while (true) {
-            try {
-                String toX = scanner.nextLine().trim();
-                if (toX.isEmpty()) {
-                    System.err.println("Введите снова, координата не может быть пустой");
-                    continue;
-                }
-                toXcoordinate = Long.parseLong(toX);
-                break;
-            } catch (NumberFormatException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты X.");
-            }
-        }
-        int toYcoordinate;
-        System.out.print("Введите координату to (Y): ");
-        while (true) {
-            try {
-                String toY = scanner.nextLine().trim();
-                if (toY.isEmpty()) {
-                    System.err.println("Введите снова, координата не может быть пустой");
-                    continue;
-                }
-                toYcoordinate = Integer.parseInt(toY);
-                break;
-            } catch (NumberFormatException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты Y.");
-            }
-        }
-        double toZcoordinate = 0;
-        System.out.print("Введите координату to (Z): ");
-        while (true) {
-            try {
-                String toZ = scanner.nextLine().trim();
-                if (toZ.isEmpty()) {
-                    break;
-                }
-                toZcoordinate = Double.parseDouble(toZ);
-                break;
-            } catch (NumberFormatException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты Z.");
-            }
-        }
-        String toName ;
-        System.out.print("Введите название to (может быть null, но не пустым): ");
-        while (true) {
-            toName = scanner.nextLine().trim();
-            if (!toName.isEmpty()) {
-                break;
-            } else {
-                System.err.println("Название не может быть пустым. Попробуйте снова.");
-            }
-        }
-        LocationTo locationTo = null;
         try {
-            locationTo = new LocationTo(toXcoordinate, toYcoordinate, toZcoordinate, toName);
-        } catch (NumberFormatException e) {
-            System.err.println("Ошибка при создании LocationTo: " + e.getMessage());
-        }
-        double distance2 ;
-        System.out.print("Введите дистанцию маршрута (> 1): ");
-        while (true) {
-            try {
-                distance2 = scanner.nextDouble();
-                if (distance2 <= 1) {
-                    System.err.println("Ошибка: дистанция должна быть больше 1. Попробуйте снова.");
-                    continue;
-                }
-                break;
-            } catch (InputMismatchException e) {
-                System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для дистанции.");
-                scanner.nextLine();
+            System.out.print("Введите название маршрута: ");
+            String name = scanner.nextLine().trim();
+            while (name.isEmpty() || name.equals(" ")) {
+                System.out.println(("Error: Название не может быть пустым/пробелом. Введите снова: "));
+                name = scanner.nextLine();
             }
+            int x;
+
+            System.out.print("Введите координату X (целое число > -372): ");
+            while (true) {
+                try {
+                    x = scanner.nextInt();
+                    if (x <= -372) {
+                        System.err.print("Число должно быть больше -372. Попробуйте снова.");
+                        continue;
+                    }
+                    System.out.println("Вы ввели корректное число: " + x);
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
+                    scanner.nextLine();
+                }
+            }
+            float y;
+            System.out.print("Введите координату Y (целое число > -527): ");
+            while (true) {
+                try {
+                    y = scanner.nextFloat();
+                    if (y <= -527) {
+                        System.err.println("Число должно быть больше -527. Попробуйте снова.");
+                        continue;
+                    }
+                    System.out.println("Вы ввели корректное число: " + y);
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
+                    scanner.nextLine();
+                }
+            }
+            Coordinates coordinates2 = new Coordinates(x, y);
+            System.out.print("Введите координату from (X): ");
+            double fromX;
+            while (true) {
+                try {
+                    fromX = scanner.nextDouble();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
+                    scanner.nextLine();
+                }
+            }
+            System.out.print("Введите координату from (Y): ");
+            int fromY;
+            while (true) {
+                try {
+                    fromY = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное число.");
+                    scanner.nextLine();
+                }
+            }
+            System.out.print("Введите точку начала маршрута (до 956 символов): ");
+            scanner.nextLine();
+            String fromName = scanner.nextLine().trim();
+            while (fromName.isEmpty() || (fromName.length() > 956)) {
+                System.err.print("Ошибка! Введите корректное точку начала маршрута: ");
+                fromName = scanner.nextLine();
+            }
+            LocationFrom locationFrom = new LocationFrom(fromX, fromY, fromName);
+            long toXcoordinate;
+            System.out.print("Введите координату to (X): ");
+            while (true) {
+                try {
+                    String toX = scanner.nextLine().trim();
+                    if (toX.isEmpty()) {
+                        System.err.println("Введите снова, координата не может быть пустой");
+                        continue;
+                    }
+                    toXcoordinate = Long.parseLong(toX);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты X.");
+                }
+            }
+            int toYcoordinate;
+            System.out.print("Введите координату to (Y): ");
+            while (true) {
+                try {
+                    String toY = scanner.nextLine().trim();
+                    if (toY.isEmpty()) {
+                        System.err.println("Введите снова, координата не может быть пустой");
+                        continue;
+                    }
+                    toYcoordinate = Integer.parseInt(toY);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты Y.");
+                }
+            }
+            double toZcoordinate = 0;
+            System.out.print("Введите координату to (Z): ");
+            while (true) {
+                try {
+                    String toZ = scanner.nextLine().trim();
+                    if (toZ.isEmpty()) {
+                        break;
+                    }
+                    toZcoordinate = Double.parseDouble(toZ);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для координаты Z.");
+                }
+            }
+            String toName;
+            System.out.print("Введите название to (может быть null, но не пустым): ");
+            while (true) {
+                toName = scanner.nextLine().trim();
+                if (!toName.isEmpty()) {
+                    break;
+                } else {
+                    System.err.println("Название не может быть пустым. Попробуйте снова.");
+                }
+            }
+            LocationTo locationTo = null;
+            try {
+                locationTo = new LocationTo(toXcoordinate, toYcoordinate, toZcoordinate, toName);
+            } catch (NumberFormatException e) {
+                System.err.println("Ошибка при создании LocationTo: " + e.getMessage());
+            }
+            double distance2;
+            System.out.print("Введите дистанцию маршрута (> 1): ");
+            while (true) {
+                try {
+                    distance2 = scanner.nextDouble();
+                    if (distance2 <= 1) {
+                        System.err.println("Ошибка: дистанция должна быть больше 1. Попробуйте снова.");
+                        continue;
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Ошибка: введено не число. Пожалуйста, введите корректное значение для дистанции.");
+                    scanner.nextLine();
+                }
+            }
+            return new Route(idLast, name, coordinates2, locationFrom, locationTo, distance2);
+        } catch (NoSuchElementException e) {
+            System.err.println("Неверный ввод. Повторите снова");
+            return null;
         }
-        return new Route(idLast, name, coordinates2, locationFrom, locationTo, distance2);
     }
     @Override
     public String toString() {
